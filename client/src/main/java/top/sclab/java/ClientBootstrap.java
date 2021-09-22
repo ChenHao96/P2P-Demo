8b5b589d74b8581741fa255d8123daa69dfba771
Steven-Work <949727157@qq.com>
2021-09-22 16:19:09 +0800

    add TCP demo

--- client/src/main/java/top/sclab/java/ClientBootstrap.java
+++ client/src/main/java/top/sclab/java/ClientBootstrap.java
@@ -1,11 +1,74 @@
 package top.sclab.java;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.Enumeration;
+import java.util.concurrent.CountDownLatch;
 
 public class ClientBootstrap {
 
-    public static void main(String[] args) throws SocketException {
+    private static final class ObjectStore<T> {
+
+        private T value;
+
+        public T get() {
+            return value;
+        }
+
+        public void set(T value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String port = System.getProperty("port", "8880");
+        String host = System.getProperty("host", "localhost");
+        InetSocketAddress serverAddress = new InetSocketAddress(host, Integer.parseInt(port));
+
+        ObjectStore<SocketAddress> objectStore = new ObjectStore<>();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket();
+                socket.setReuseAddress(true);
+                socket.connect(serverAddress);
+                objectStore.set(socket.getLocalSocketAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            SocketAddress socketAddress = objectStore.get();
+            if (socketAddress == null) {
+                System.out.println("SocketAddress is null.");
+                return;
+            } else {
+                System.out.println(socketAddress);
+            }
+
+            try {
+                ServerSocket serverSocket = new ServerSocket();
+                serverSocket.setReuseAddress(true);
+                serverSocket.bind(socketAddress, 2);
+                serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void main2(String[] args) throws SocketException {
 
         Enumeration<NetworkInterface> faces = NetworkInterface.getNetworkInterfaces();
         while (faces.hasMoreElements()) {
